2dfdc11a262b88836b8e02736825e14df5bc45d9
zalanddmi <zalanddmi@gmail.com>
2023-05-07 10:07:07 +0500

    Add organizations classes

---
+++ MedicalExamination/Models/Organization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalExamination.Models
+{
+    public class Organization
+    {
+        public string Name;
+        public string TaxIdNumber;
+        public string CodeReason;
+        public string Address;
+        public bool IsJuridicalPerson;
+        public TypeOrganization TypeOrganization;
+
+        public Organization(string name, string taxIdNumber, string codeReason, string address, 
+            bool isJuridicalPerson, TypeOrganization typeOrganization)
+        {
+            Name = name;
+            TaxIdNumber = taxIdNumber;
+            CodeReason = codeReason;
+            Address = address;
+            IsJuridicalPerson = isJuridicalPerson;
+            TypeOrganization = typeOrganization;
+        }
+    }
+
+    public class TypeOrganization
+    {
+        public string Name;
+
+        public TypeOrganization(string name)
+        {
+            Name = name;
+        }
+    }
+}
