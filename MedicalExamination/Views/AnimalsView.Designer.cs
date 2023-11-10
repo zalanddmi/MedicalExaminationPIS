20e00e13c72e583d37590283bb7b0a5e5ce836d8
Mcore-01 <dassd2123@gmail.com>
2023-11-10 20:00:11 +0500

    поменял фон в регистре животных, сделал центрирование форм конрактов

--- MedicalExamination/Views/AnimalsView.Designer.cs
+++ MedicalExamination/Views/AnimalsView.Designer.cs
@@ -69,6 +69,7 @@ namespace MedicalExamination.Views
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.IdAnimal,
