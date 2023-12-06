1735cab214c99076f8baa8a220d9903fa7f08552
yanpti <cimdroid360@gmail.com>
2023-12-06 23:43:46 +0500

    Создала класс Report и к нему Repository, Service, Controller

---
+++ ServerME/Models/Report.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerME.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public User Creator { get; set; }
+        public string FilePath { get; set; }
+        public string Status { get; set; }
+        public DateTime StatusDate { get; set; }
+        public Report()
+        {
+
+        }
+    }
+}
